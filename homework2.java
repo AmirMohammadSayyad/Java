742b6cf6936eade5b5ba52abf6f8eba70643eb2c
Amir Mohammad <amirmohamadsyd1384@gmail.com>
2025-04-21 16:59:03 +0330

    Add files via upload

---
+++ homework2.java
@@ -0,0 +1,8 @@
+public class homework{
+  public static void main(String[] args){
+    System.out.println("fname: Amir Mohammad");
+    System.out.println("lname: Sayyad");
+    System.out.println("student number: 03110142302012");
+    System.out.println("course: mobile programming 1");
+  }
+}
